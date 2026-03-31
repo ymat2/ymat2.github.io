c25112c25e25e02958298ec18b6a9f65dfcbafa1
ymat2 <ymat2@users.noreply.github.com>
2026-03-31 14:27:52 +0900

    style: Remove before-toc class

--- static/before-toc.js
+++
@@ -1,9 +0,0 @@
-<script>
-document.addEventListener("DOMContentLoaded", function () {
-  let toc = document.getElementById("TOC");
-  let beforeToc = document.querySelector(".before-toc");
-  if (toc && beforeToc) {
-    toc.parentNode.insertBefore(beforeToc, toc);
-  }
-});
-</script>
